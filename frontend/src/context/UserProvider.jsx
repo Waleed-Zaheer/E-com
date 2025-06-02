bf46ee1dee6d83abda77e4fb0165d78179c4258a
Waleed-Zaheer <shahzaibbutt920@gmail.com>
2025-06-02 15:46:07 +0500

    cart fixes and logout(Storages clearing)

--- frontend/src/context/UserProvider.jsx
+++ frontend/src/context/UserProvider.jsx
@@ -1,6 +1,5 @@
 import React, { createContext, useState, useContext, useEffect } from 'react';
 import axios from 'axios';
-import { Cookie } from 'lucide-react';
 
 const UserContext = createContext({
   user: null,
